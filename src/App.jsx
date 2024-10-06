b31c57f4051e9d9fa3278994cba945261c170145
Pedropereira77 <52599338+Pedropereira77@users.noreply.github.com>
2024-10-06 01:09:57 +0100

    Primeiro commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Header from "./components/HEADER/Header.jsx";
+import CoreConcepts from "./components/CoreConcepts.jsx";
+import Examples from "./components/Examples.jsx";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <main>
+        <CoreConcepts />
+        <Examples />
+      </main>
+    </>
+  );
+}
+
+export default App;
